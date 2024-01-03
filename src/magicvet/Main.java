b672f8cfb2f262ce8b0bf692cf069af84dd87d75
RomanStas <roman.stasik@gmail.com>
2024-01-03 20:04:58 +0200

    Firstname/Lastname validation

--- src/magicvet/Main.java
+++ src/magicvet/Main.java
@@ -10,6 +10,7 @@ public class Main {
     static String PASSWORD = "default";
     static Scanner SCANNER = new Scanner(System.in);
     static String EMAIL_PATTERN = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    static String NAME_PATTERN = "[A-Z][a-z]*-?([A-Z][a-z]*)?";
 
     public static void main(String[] args) {
         run();
@@ -42,24 +43,34 @@ public class Main {
         String email = SCANNER.nextLine();
 
         if (isEmailValid(email)) {
-            Client client = buildClient(email);
-            System.out.println("New client: " + client.firstName
-                    + " " + client.lastName
-                    + " (" + client.email + ")");
+            System.out.println("First name (with first uppercase letter in each word): ");
+            String firstName = SCANNER.nextLine();
+
+            if (isNameValid(firstName)) {
+                System.out.println("Last name (with first uppercase letter in each word): ");
+                String lastName = SCANNER.nextLine();
+
+                if (isNameValid(lastName)) {
+                    Client client = buildClient(firstName, lastName, email);
+                    System.out.println("New client: " + client.firstName
+                            + " " + client.lastName
+                            + " (" + client.email + ")");
+                } else {
+                    System.out.println("Provided last name is invalid.");
+                }
+            } else {
+                System.out.println("Provided first name is invalid.");
+            }
         } else {
             System.out.println("Provided email is invalid.");
         }
     }
 
-    static Client buildClient(String email) {
+    static Client buildClient(String firstName, String lastName, String email) {
         Client client = new Client();
         client.email = email;
-
-        System.out.println("First name: ");
-        client.firstName = SCANNER.nextLine();
-
-        System.out.println("Last name: ");
-        client.lastName = SCANNER.nextLine();
+        client.firstName = firstName;
+        client.lastName = lastName;
 
         return client;
     }
@@ -67,6 +78,12 @@ public class Main {
     static boolean isEmailValid(String email) {
         Pattern pattern = Pattern.compile(EMAIL_PATTERN);
         Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    static boolean isNameValid(String name) {
+        Pattern pattern = Pattern.compile(NAME_PATTERN);
+        Matcher matcher = pattern.matcher(name);
         return matcher.matches();
     }
 }
