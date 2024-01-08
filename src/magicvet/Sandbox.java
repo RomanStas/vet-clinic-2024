8722373b5efed9d0618c138dae60bab5255a31c7
RomanStas <roman.stasik@gmail.com>
2024-01-08 21:27:12 +0200

    REQ-06: Sort dogs by size

---
+++ src/magicvet/Sandbox.java
@@ -0,0 +1,26 @@
+package magicvet;
+
+import magicvet.comparator.DogSizeComparator;
+import magicvet.model.Dog;
+
+import java.util.Arrays;
+
+public class Sandbox {
+    public static void main(String[] args) {
+        Dog[] dogs = {
+                new Dog(Dog.M),
+                new Dog(Dog.S),
+                new Dog(Dog.XL),
+                new Dog(Dog.XL),
+                new Dog(Dog.XS),
+                new Dog(Dog.S)
+        };
+
+        Arrays.sort(dogs, new DogSizeComparator());
+
+        for (Dog dog : dogs) {
+            System.out.println(dog.getSize());
+        }
+
+    }
+}
--- src/magicvet/model/Dog.java
+++ src/magicvet/model/Dog.java
@@ -4,32 +4,17 @@ import java.util.Objects;
 
 public class Dog extends Pet {
 
+    public static final String XS = "XS";
+    public static final String S = "S";
+    public static final String M = "M";
+    public static final String L = "L";
+    public static final String XL = "XL";
+
     private String size;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        Dog dog = (Dog) o;
-        return Objects.equals(size, dog.size);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), size);
-    }
-
-    @Override
-    public String toString() {
-        return "Pet {"
-                + "type = " + this.getType()
-                + ", sex = " + super.getSex()
-                + ", age = " + super.getAge()
-                + ", name = " + super.getName()
-                + ", ownerName = " + super.getOwnerName()
-                + ", size = " + this.getSize()
-                + "}";
+    public Dog() {}
+    public Dog(String size) {
+        this.size = size;
     }
 
     public void setSize(String size) {
